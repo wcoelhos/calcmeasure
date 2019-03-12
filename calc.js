b760a335b54834cc1c338f85326deba4326e5998
PeopleRPM <34247686+PeopleRPM@users.noreply.github.com>
2019-03-12 18:57:57 -0300

    Initial commit

---
+++ calc.js
@@ -0,0 +1,146 @@
+class Area {
+
+	constructor(target) {
+		this.areas = []
+
+		var html = ''
+			html += '<div class="calcarea">'
+			html += '	<div class="calcarea-header">'
+			html += '		Calcule abaixo quantos m² você precisa:'
+			html += '	</div>'
+			html += '	<form action="javascript:void(0)" class="calcarea-form">'
+			html += '		<div>'
+			html += '			<label>Altura</label>'
+			html += '			<input type="text" name="altura" placeholder="0,00">'
+			html += '		</div>'
+			html += '		<div>'
+			html += '			<label>Comprimento</label>'
+			html += '			<input type="text" name="comprimento" placeholder="0,00">'
+			html += '		</div>'
+			html += '		<div>'
+			html += '			<label>--</label>'
+			html += '			<button type="submit">Adicionar</button>'
+			html += '		</div>'
+			html += '	</form>'
+			html += '	<ul class="calcarea-list list">'
+			html += '		<li style="display: none;">'
+			html += '			<a href="javascript:void(0)" class="remove">x</a>'
+			html += '			<span class="altura">000</span>m Altura x <span class="comprimento">000</span>m Comprimento <strong>(<span class="area">00m²</span>)</strong>'
+			html += '		</li>'
+			html += '	</ul>'
+			html += '</div>'
+
+		this.calcarea = $(html)
+		this.calcareaForm = $(this.calcarea).find('.calcarea-form')
+		this.inputAltura = $(this.calcareaForm).find('input[name="altura"]')
+		this.inputComprimento = $(this.calcareaForm).find('input[name="comprimento"]')
+		this.calcareaListElement = $(this.calcarea).find('.calcarea-list')
+		this.baseCalcareaElement = $(this.calcareaListElement).find(' > li:hidden').first().clone().css('display', 'block')
+		this.changeFunc = null
+
+		$(this.inputAltura).maskMoney({thousands:'.', decimal:','});
+		$(this.inputComprimento).maskMoney({thousands:'.', decimal:','});
+
+		$(target).append(this.calcarea)
+		this.start()
+	}
+
+	addCalcareaItem (area, index) {
+		var _self = this
+
+		var item = $(this.baseCalcareaElement).clone()
+			item.find('.altura').text(this.formatNumber(area.altura))
+			item.find('.comprimento').text(this.formatNumber(area.comprimento))
+			item.find('.area').text(this.formatNumber(area.area) + 'm²')
+			item.find('.remove').click(function () {
+				_self.removeArea(index)
+			})
+		
+		$(this.calcareaListElement).prepend(item)
+	}
+
+	renderAreas () {
+		var _self = this
+		
+		$(this.calcareaListElement).find('> li').remove()
+
+		this.areas.forEach(function (area, index) {
+			_self.addCalcareaItem(area, index)
+		})
+
+		if (this.changeFunc) {
+			this.changeFunc(this.areas)
+		}
+	}
+
+	addArea (area) {
+		this.areas.push(area)
+		this.renderAreas()
+	}
+
+	removeArea (index) {
+		this.areas.splice(index, 1)
+		this.renderAreas()
+	}
+
+	parseNumber (num) {
+		var index = num.lastIndexOf(".") > num.lastIndexOf(",") ? num.lastIndexOf(".") : num.lastIndexOf(",")
+		
+		if (index >= 0) {
+			var number = num.substring(0, index)
+			var decimal = num.substring(index+1)
+			num = number.replace('.', '').replace(',', '') + '.' + decimal.replace('.', '').replace(',', '')
+		}
+
+		return $.isNumeric(num) ? parseFloat(num) : null
+	}
+
+	formatNumber (num) {
+		num = parseFloat(parseFloat(num).toFixed(2))
+		return num.toLocaleString(
+		  'pt-BR',
+		  { minimumFractionDigits: 2 }
+		)
+	}
+
+	submit (form) {
+		var _self = this
+		var valAltura = _self.parseNumber(this.inputAltura.val())
+		var valComprimento = _self.parseNumber(this.inputComprimento.val())
+
+		if (valAltura && valComprimento) {
+			_self.addArea({
+				altura: valAltura,
+				comprimento: valComprimento,
+				area: Math.round(valAltura * valComprimento)
+			})
+			_self.inputAltura.removeClass('input-error').val('')
+			_self.inputComprimento.removeClass('input-error').val('')
+		} else {
+			if (!valAltura) _self.inputAltura.addClass('input-error')
+			if (!valComprimento) _self.inputComprimento.addClass('input-error')
+		}
+	}
+
+	getAreas () {
+		return this.areas
+	}
+
+	change (func) {
+		this.changeFunc = func
+	}
+
+	start (target) {
+		var _self = this
+		$(this.calcareaForm).submit(function (el) {
+			_self.submit()
+		})
+		$(_self.inputAltura).keypress(function (el) {
+			$(el.target).removeClass('input-error')
+		})
+		$(_self.inputAltura).keypress(function (el) {
+			$(el.target).removeClass('input-error')
+		})
+		this.renderAreas()
+	}
+}
---
+++ calcAction.js
@@ -0,0 +1,156 @@
+class enableCalcPage {
+ 
+	constructor(target) {
+		this.calc = new Area('.info-principal-produto')
+
+		var _self = this
+		$('.botao.botao-comprar.principal.grande:not(.desativo)').click(function(e) {
+			_self.recordAreas(e.target)
+		})//.attr('href', '#')
+
+		this.calc.change(function (areas) {
+			_self.applyQtd(areas)
+		})
+	}
+
+	recordAreas (target) {
+		var sku = $(target).parents('.acoes-produto').find('meta[itemprop=sku]').prop('content')
+		
+		if (sku) {
+			var areas = JSON.stringify(this.calc.getAreas())
+			localStorage.setItem(sku, areas)
+			console.log('Adicinado:', sku, areas)
+		}
+	}
+
+	applyQtd (areas) {
+		var qtdInput = $('.principal .comprar .qtde-adicionar-carrinho input[name=qtde-carrinho]')
+
+		if (areas.length) {
+			 var sum = areas
+			.map(function (val) {
+				return val.area
+			})
+			.reduce(function (accumulator, currentValue) {
+				return accumulator + currentValue
+			})
+		} else {
+			var sum = 0
+		}
+		qtdInput.val(sum)
+		qtdInput.change()
+	}
+}
+
+class enableCheckcoutPage {
+
+	constructor() {
+		this.observationsInput = $('textarea[name=cliente_obs]').first()
+		this.observationsInput.val(this.getTextAreas())
+
+		$(this.observationsInput).parents('.caixa-sombreada').css('opacity', '0.3')
+	}
+
+	getCheckoutSkus () {
+		var items = []
+		$('.tabela-carrinho .produto-info > ul').each(function (index, element) {
+			var sku = $(element).find('>li').first().find('strong').text().trim()
+			if (sku) items.push(sku)
+		})
+		return items
+	}
+
+	getAreasFromSkus () {
+		var checkoutSkus = this.getCheckoutSkus()
+
+		var items = []
+		checkoutSkus.forEach(function (sku, index) {
+			var areas = localStorage.getItem(sku)
+
+			if (sku && areas) {
+				items.push({
+					sku: sku,
+					areas: JSON.parse(areas)
+				})
+			}
+		})
+		return items
+	}
+
+	getTextAreas () {
+		var _self = this
+		var skus = this.getAreasFromSkus()
+
+		return skus.map(function (val) {
+			var areas = val.areas.map(function (area) {
+				return ""+_self.formatNumber(area.altura)+"m x "+_self.formatNumber(area.comprimento)+"m ("+_self.formatNumber(area.area)+"m)"
+			}).join("\r\n")
+
+			return [
+				"SKU: "+val.sku,
+				areas
+			].join("\r\n")
+
+		}).join("\r\n\r\n")
+	}
+
+	formatNumber (num) {
+		num = parseFloat(parseFloat(num).toFixed(2))
+		return num.toLocaleString(
+		  'pt-BR',
+		  { minimumFractionDigits: 2 }
+		)
+	}
+
+}
+
+class checkPageType {
+
+	constructor() {
+	}
+
+	isCalc () {
+		var title = $(".nome-produto.titulo:contains('Sob Medida')")
+		return $(title).length && this.sandbox()
+	}
+
+	isCheckcout () {
+		return $('textarea[name=cliente_obs]').length && this.sandbox()
+	}
+
+	sandbox () {
+		var url = new URL(window.location.href);
+		var test = url.searchParams.get("test");
+
+		return test==='calc'
+	}
+}
+
+
+$(document).ready(function () {
+
+	var pageType = new checkPageType
+
+	if (pageType.isCalc()) {
+		console.log('isCalcPage')
+		new enableCalcPage
+	}
+	if (pageType.isCheckcout()) {
+		console.log('isCheckcoutPage')
+		new enableCheckcoutPage
+	}
+
+	
+	if (pageType.sandbox()) {
+		$('.calc-gismar02').hide()
+
+		/*
+		$('a').each(function (index, item) {
+			var url = new URL($(item).prop('href'));
+			url.searchParams.set("test", "calc");
+			$(item).prop('href', url)
+		})
+		*/
+	}
+
+})
